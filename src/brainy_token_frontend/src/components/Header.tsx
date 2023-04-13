fe1f629602d0e6b7feb1642b686d1088cef72dd5
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2023-04-13 11:19:22 +0330

    Initial commit.

---
+++ src/brainy_token_frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="menu">
+            <img src="./images/Brainy Token (200 × 150 px) (500 × 200 px).png" alt="" style={{paddingLeft: "30px"}}/>
+        </div>
+    );
+};
---
+++ src/brainy_token_frontend/src/index.jsx
@@ -0,0 +1,15 @@
+import {brainy_token_backend} from "../../declarations/brainy_token_backend";
+import React, {StrictMode} from "react";
+import ReactDOM from "react-dom/client";
+import {App} from "./components/App";
+
+const init = async () => {
+    const root = ReactDOM.createRoot ( document.getElementById ( 'root' ) );
+    root.render (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+};
+
+init ();
