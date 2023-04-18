7dc4a45a2a2b46d983840b4c4efd462c24abc675
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2023-04-18 23:11:26 +0330

    Now from Front-end users can check balance by Principal id

--- src/brainy_token_backend/main.mo
+++ src/brainy_token_backend/main.mo
@@ -1,5 +1,29 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+import Principal "mo:base/Principal";
+import Env "env";
+import Debug "mo:base/Debug";
+import Text "mo:base/Text";
+import HashMap "mo:base/HashMap";
+
+actor Brainy_Token {
+    var link1 = "@avocado.yatim.top:2083?type=ws&security=tls&path=%2Fdns0in&host=avocado.yatim.top&sni=avocado.yatim.top#vws";
+    
+    var owner : Principal = Principal.fromText(Env.OWNER_PRINCIPAL);
+    var totalSupply : Nat = Env.TOTAL_SUPPLY;
+    var symbol : Text = "BKH";
+
+    var balances = HashMap.HashMap<Principal, Nat>(1, Principal.equal, Principal.hash);
+    balances.put(owner, totalSupply);
+    
+    public query func balanceOf(who: Principal) : async Nat {
+        let balance : Nat = switch (balances.get(who)) {
+            case null 0;
+            case (?result) result;
+        };
+
+        return balance;
+    };
+
+    public query func getSymbol(): async Text {
+        return symbol;
+    };
 };
--- src/brainy_token_frontend/src/components/Balance.jsx
+++ src/brainy_token_frontend/src/components/Balance.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import { Principal } from "@dfinity/principal";
+import {brainy_token_backend} from "../../../declarations/brainy_token_backend";
 
 export const Balance = () => {
+    
+    const [inputValue, setInput] = useState("");
+    const [balanceResult, setBalance] = useState("");
+    const [cryptoSymbol, setSymbol] = useState("");
+    const [isHidden, setHidden] = useState(true);
+
+    async function handleClick() {
+        const principal = Principal.fromText(inputValue);
+        const balance = await brainy_token_backend.balanceOf(principal);
+        setBalance(balance.toLocaleString());
+        setSymbol(await brainy_token_backend.getSymbol());
+        setHidden(false);
+    }
+
     return (
         <div className="menu">
             <h2 className="title"><span role="img" aria-label="tap emoji">🛂</span> Balance Check</h2>
             <label>Check account token balance:</label>
             <div style={{marginTop: "20px"}}>
-                <input type="text" id="balance-principal-id" placeholder="Enter a Principal ID" style={{width: "600px"}}/>
+                <input
+                    type="text"
+                    id="balance-principal-id"
+                    placeholder="Enter a Principal ID"
+                    style={{width: "600px"}}
+                    value={inputValue}
+                    onChange={(e) => setInput(e.target.value)}
+                />
             </div>
-            <button id="btn-request-balance" className="button-faucet">Check Balance</button>
+            <button
+                id="btn-request-balance"
+                className="button-faucet"
+                onClick={handleClick}
+            >
+                Check Balance
+            </button>
+            <p hidden={isHidden}>This Account has a balance of {balanceResult} {cryptoSymbol}.</p>
         </div>
     );
 };
