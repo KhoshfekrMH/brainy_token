18265f7e3ce178e4e0ecb4066af1bae46afe9fed
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2023-04-24 20:59:56 +0330

    Transfer function added for Supply token

--- src/brainy_token_frontend/src/components/Transfer.jsx
+++ src/brainy_token_frontend/src/components/Transfer.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import {brainy_token_backend} from "../../../declarations/brainy_token_backend";
+import {Principal} from "@dfinity/principal";
+
 export const Transfer = () => {
+
+    const [recipientId, setId] = useState("");
+    const [amount, setAmount] = useState("");
+    const [isDisabled, setDisabled] = useState(false);
+    const [feedback, setFeedback] = useState("");
+    const [isHidden, setHidden] = useState(true);
+
+    async function handleClick() {
+        setHidden(true);
+        setDisabled(true);
+        const recipient = Principal.fromText(recipientId);
+        const amountToTransfer = Number(amount);
+        const result = await brainy_token_backend.transfer(recipient, amountToTransfer);
+        setFeedback(result);
+        setHidden(false);
+        setDisabled(false);
+    }
+
     return (
         <div className="menu">
             <h2 className="title"><span role="img" aria-label="tap emoji">🔄</span> Transfer</h2>
@@ -8,7 +29,12 @@ export const Transfer = () => {
                     <legend>To Account:</legend>
                     <ul>
                         <li>
-                            <input type="text" id="transfer-to-id"/>
+                            <input
+                                type="text"
+                                id="transfer-to-id"
+                                value={recipientId}
+                                onChange={(e) => setId(e.target.value)}
+                            />
                         </li>
                     </ul>
                 </fieldset>
@@ -16,12 +42,24 @@ export const Transfer = () => {
                     <legend>Amount:</legend>
                     <ul>
                         <li>
-                            <input type="number" id="amount"/>
+                            <input
+                                type="number"
+                                id="amount"
+                                value={amount}
+                                onChange={(e) => setAmount(e.target.value)}
+                            />
                         </li>
                     </ul>
                 </fieldset>
             </div>
-            <button className="button-faucet">Transfer</button>
+            <button
+                className="button-faucet"
+                onClick={handleClick}
+                disabled={isDisabled}
+            >
+                Transfer
+            </button>
+            <p hidden={isHidden}>{feedback}</p>
         </div>
     );
 };
